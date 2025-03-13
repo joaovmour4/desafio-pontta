a4d77b435bfdf22f70f091f275f18b83bb4c36ac
joaovmour4 <joaov.mour@gmail.com>
2025-03-13 16:52:49 -0300

    configurando ambiente

---
+++ backend/src/main/java/com/pontta/hardware_shopping/HardwareShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.pontta.hardware_shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HardwareShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HardwareShoppingApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/pontta/hardware_shopping/HardwareShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontta.hardware_shopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HardwareShoppingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
